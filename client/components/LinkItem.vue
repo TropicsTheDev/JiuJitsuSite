ed9cbb43c969f38e12b8fe34bf70d1102e13fee8
Douglas Townsend <dougymt@gmail.com>
2023-10-25 01:54:39 -0400

    separate files for list and list-items

---
+++ client/components/LinkItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <li class="link" :class="{ 'link--open': open }">
+    <h3 class="link__heading">{{ link.title }}</h3>
+    <div class="link__wrapper">
+      <a :href="link.link" target="_blank">{{ link.text }}</a>
+    </div>
+  </li>
+</template>
+
+<script setup>
+const props = defineProps(["link", "open"]);
+</script>
+
+<style lang="scss">
+.link {
+  background-color: $color_base;
+  width: 75%;
+  text-align: center;
+  padding: 0;
+  border-radius: 0.5em;
+  color: white;
+
+  &__heading {
+    margin-bottom: 0;
+    padding: 0.5em;
+  }
+
+  &__wrapper a {
+    display: block;
+    background-color: $color_accent;
+    background-color: #f5e4f6;
+    /* color: $color_white; */
+    color: #333;
+    font-size: var(--font-size_lg);
+    max-height: 0;
+    overflow: hidden;
+    transition: max-height 1s ease-in-out, padding 0.5s ease-in-out;
+  }
+
+  &--open &__wrapper a {
+    max-height: calcRem(300);
+    padding: 1em;
+    box-shadow: inset 0px calcRem(9) calcRem(16) #{$color_background}80;
+  }
+}
+</style>
---
+++ client/components/LinkList.vue
@@ -0,0 +1,46 @@
+<template>
+  <ul>
+    <LinkItem
+      v-for="(link, index) in links"
+      @click="setActiveLinkIndex(index)"
+      :link="link"
+      :open="index === activeLinkIndex"
+    />
+  </ul>
+</template>
+
+<script setup>
+const links = ref([
+  {
+    title: "Schedule a private lesson!",
+    text: "Click here to book a one on one private lesson with me",
+    link: "https://calendly.com/dtownjiujitsu/privatelesson",
+  },
+  {
+    title: "Get discounted Jiu-Jitsu gear",
+    text: "Click here to get 15% off ProtectYaNeckMMA products",
+    link: "https://invi.tt/F6AP",
+  },
+]);
+
+const activeLinkIndex = ref(null);
+
+function setActiveLinkIndex(index) {
+  if (index === activeLinkIndex.value) {
+    activeLinkIndex.value = null;
+  } else {
+    activeLinkIndex.value = index;
+  }
+}
+</script>
+
+<style lang="scss">
+ul {
+  @include center-self;
+  @include center-content;
+  padding: 0;
+  list-style: none;
+  width: 100%;
+  row-gap: var(--font-size_lg);
+}
+</style>
